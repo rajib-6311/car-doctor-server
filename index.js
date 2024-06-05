e80d2574c91962100ae00268099f60edf448f6b4
rajibhossen <mhossen6311@gmail.com>
2024-06-05 23:21:31 +0600

    Services added

--- index.js
+++ index.js
@@ -27,13 +27,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+      await client.connect();
+
+      const ServiceCollection = client.db('carDoctor').collection('Services');
+
+      app.get('/Services', async (req, res) => {
+          const cursor = ServiceCollection.find();
+          const result = await cursor.toArray();
+          res.send(result);
+      })
+      
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
